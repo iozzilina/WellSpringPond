056d593b3c5641c15faf31beb869f7edf50a1d6f
iozzilina <iova_dineva@yahoo.com>
2017-05-10 12:34:23 +0300

    Html validation attributes

--- source/WellSpringPond.Models/ViewModels/WaterSources/WaterSourceQuickAddVm.cs
+++ source/WellSpringPond.Models/ViewModels/WaterSources/WaterSourceQuickAddVm.cs
@@ -1,14 +1,22 @@
 ﻿namespace WellSpringPond.Models.ViewModels.WaterSources
 {
+    using System.ComponentModel.DataAnnotations;
     using System.Collections.Generic;
 
     public class WaterSourceQuickAddVm
     {
+        [Required(ErrorMessage = "Please enter a name.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter the type of the watersource.")]
         public string Type { get; set; }
-        
+
+        [Required(ErrorMessage = "Please enter a Latitude")]
+        [Range(-90.00, 90.00, ErrorMessage = "Please enter a valid Latitude")]
         public decimal Latitude { get; set; }
+
+        [Required(ErrorMessage = "Please enter valid Longtitude")]
+        [Range(-180.00, 180.00, ErrorMessage = "Please enter a valid Longtitude")]
         public decimal Longitude { get; set; }
 
         public bool IsDrinkable { get; set; }
